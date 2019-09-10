702f6e494a28c0d6620c4e534207a8225bedc809
Donald Fossouo <dfossouo@hortonworks.com>
2019-09-10 17:18:58 +0200

    update producer to take in consideration props file

--- src/main/scala/com/dfossouo/scala/k2hdfs/Producer.scala
+++ src/main/scala/com/dfossouo/scala/k2hdfs/Producer.scala
@@ -1,20 +1,41 @@
 package com.dfossouo.scala.k2hdfs
 
+import com.dfossouo.scala.k2hdfs.ConsumerStreaming.getProps
+
+import scala.collection.mutable.HashMap
+import scala.io.Source.fromFile
+
 object Producer extends App {
 
   import java.util.Properties
 
   import org.apache.kafka.clients.producer._
 
-  val  props = new Properties()
-  props.put("bootstrap.servers", "c225-node2.squadron-labs.com:6667,c225-node3.squadron-labs.com:6667,c225-node4.squadron-labs.com:6667")
+  def getProps(file: => String): HashMap[String, String] = {
+    var props = new HashMap[String, String]
+    val lines = fromFile(file).getLines
+    lines.foreach(x => if (x contains "=") props.put(x.split("=")(0), if (x.split("=").size > 1) x.split("=")(1) else null))
+    props
+  }
 
-  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  val props = getProps(args(0))
 
-  val producer = new KafkaProducer[String, String](props)
+  val bootstrap_servers = props.get("bootstrap.servers").get
+  val key_serializer = props.get("key.serializer").get
+  val value_serializer = props.get("value.serializer").get
 
-  val TOPIC="test2"
+  val topics = props.get("topic.name").get
+
+  val  props_producer = new Properties()
+  props_producer.put("bootstrap.servers", bootstrap_servers)
+
+  props_producer.put("key.serializer", key_serializer)
+  props_producer.put("value.serializer", value_serializer)
+
+
+  val producer = new KafkaProducer[String, String](props_producer)
+
+  val TOPIC = topics
 
   for(i<- 1 to 1000000){
     val record = new ProducerRecord(TOPIC, "key", s"hi world, $i")
